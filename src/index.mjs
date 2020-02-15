23ffecde7d9a0a0cdfef1b985aa11a5ab7fb1451
Chung Leong <chernyshevsky@hotmail.com>
2020-02-15 18:43:52 +0100

    Simplified default export syntax

--- src/index.mjs
+++ src/index.mjs
@@ -3,5 +3,4 @@ export * from './route-manager-event.mjs';
 export * from './route-manager-error.mjs';
 export * from './route-manager-proxy.mjs';
 
-import { RouteManager } from './route-manager.mjs';
-export default RouteManager;
+export { RouteManager as default } from './route-manager.mjs';
